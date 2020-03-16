b38cde8f17c2ba29b2a39e100620087807a90b5d
kn0wn <admin@joshking.dev>
2020-03-16 14:20:08 +0000

    fix: added a better example page

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,32 @@
   <div id="app">
     <h1>vue-cosha</h1>
 
-    <img
-      src="https://images.unsplash.com/photo-1583542225715-473a32c9b0ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60"
-      v-cosha
-    />
-    <br />
-    <br />
-    <img
-      src="https://images.unsplash.com/photo-1557939403-1760a0e47505?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=80"
-      v-cosha
-    />
-    <br />
-    <br />
-    <img
-      src="https://images.unsplash.com/photo-1583272630857-5a0fd42e611e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=80"
-      v-cosha
-    />
+    <pre>
+  <code>
+&lt;img v-cosha src="example.jpg" /&gt;
+  </code>
+</pre>
+
+    <section class="display">
+      <aside>
+        <img
+          src="https://images.unsplash.com/photo-1583542225715-473a32c9b0ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=450&q=60"
+          v-cosha
+        />
+      </aside>
+      <aside>
+        <img
+          src="https://images.unsplash.com/photo-1557939403-1760a0e47505?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=450&q=80"
+          v-cosha
+        />
+      </aside>
+      <aside>
+        <img
+          src="https://images.unsplash.com/photo-1583272630857-5a0fd42e611e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=450&q=80"
+          v-cosha
+        />
+      </aside>
+    </section>
   </div>
 </template>
 
@@ -35,5 +45,38 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.display {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.display aside {
+  padding: 1em 2em;
+}
+
+.display aside img {
+  max-width: 80vw;
+}
+
+@media screen and (max-width: 992px) {
+  .display {
+    flex-direction: column;
+  }
+
+  pre code {
+    max-width: 85% !important;
+  }
+}
+
+pre code {
+  background-color: #eee;
+  border: 1px solid #999;
+  display: block;
+  padding: 20px;
+  max-width: 20%;
+  margin: auto;
 }
 </style>
